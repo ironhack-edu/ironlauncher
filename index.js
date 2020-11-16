3d3150076c26ed7b718b7b37c9bfdec9ba58e749
Andre <itstheandre@gmail.com>
2020-11-16 20:55:01 +0100

    creating template base

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
 console.log("hey");
+const copy = require("copy-template-dir");
+const path = require("path");
+
+const vars = { name: `awais` };
+
+const inDir = path.join(__dirname, `templates`);
+const outDir = path.join(process.cwd(), "output");
+
+copy(inDir, outDir, vars, (err, createdFiles) => {
+  if (err) throw err;
+  console.log();
+  console.log(`Creating a files in ${vars.name}`);
+
+  createdFiles.forEach((filePath) => {
+    const fileName = path.basename(filePath);
+    console.log(`Created ${fileName}`);
+  });
+  console.log();
+  console.log("DONE");
+  console.log();
+});
---
+++ templates/{{name}}.js
@@ -0,0 +1,1 @@
+// dybnamicaly generated {{name}}
