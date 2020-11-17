8dced489eb38d5e64a82f2859c0dd6bd6a6cdcc4
Filipe Freire <filipefreire@protonmail.com>
2020-11-17 18:36:26 +0100

    Add Navbar with login & signup view | Add auth router in app.js

--- template/auth/app.js
+++ template/auth/app.js
@@ -1,25 +1,28 @@
 // ℹ️ To get access to environment
-require("dotenv").config();
+require('dotenv').config();
 
 // ℹ️ Connect to the database
-require("./db");
+require('./db');
 
-const express = require("express");
-const hbs = require("hbs");
+const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require("./config")(app);
+require('./config')(app);
 
 // default value for title local
-app.locals.title = "Express - Generated with IronGenerator";
+app.locals.title = 'Express - Generated with IronGenerator';
 
 // 👇 Start handling routes here
-const index = require("./routes/index");
-app.use("/", index);
+const index = require('./routes/index');
+app.use('/', index);
+
+const auth = require('./routes/auth');
+app.use('/auth', auth);
 
 // ❗ To handle errors. Routes that dont exist or errors that you handle in specfic routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
