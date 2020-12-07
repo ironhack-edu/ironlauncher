b0822ffb578d42dbfcffd2d561daee549d41b00a
Andre <itstheandre@gmail.com>
2020-12-07 12:02:31 +0100

    Lets check

--- template/fullstack/auth/server/middlewares/shouldNotBeLoggedIn.js
+++ template/fullstack/auth/server/middlewares/shouldNotBeLoggedIn.js
@@ -4,13 +4,18 @@ module.exports = (req, res, next) => {
   // if an already logged in user tries to access the login page it
   // redirects the user to the home page
   if (req.headers.authorization) {
-    return Session.findById(req.headers.authorization).then((session) => {
-      if (session) {
-        return res.status(401).json({
-          errorMessage: "You should not be logged in to make this request",
-        });
-      }
-    });
+    return Session.findById(req.headers.authorization)
+      .then((session) => {
+        if (session) {
+          return res.status(401).json({
+            errorMessage: "You should not be logged in to make this request",
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("err", err.message);
+        return next();
+      });
   }
 
   next();
