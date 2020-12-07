44aff9ef1cefaa961243d02cf32664f34a19fbe2
Filipe Freire <filipefreire@protonmail.com>
2020-12-07 14:15:03 +0100

    -add logo to app.js | remove hbs from server required modules

--- template/fullstack/base/server/app.js
+++ template/fullstack/base/server/app.js
@@ -1,29 +1,25 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const express = require('express');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require("./config")(app);
+require('./config')(app);
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controled from the routes/index.js
-const allRoutes = require("./routes");
-app.use("/api", allRoutes);
+const allRoutes = require('./routes');
+app.use('/api', allRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
