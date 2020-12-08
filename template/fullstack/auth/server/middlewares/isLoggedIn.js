2977155f9477af28c3c3b342d3724f51e7cb88a6
Andre <itstheandre@gmail.com>
2020-12-08 14:35:07 +0100

    USER ID REMOVAL

--- template/fullstack/auth/server/middlewares/isLoggedIn.js
+++ template/fullstack/auth/server/middlewares/isLoggedIn.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   }
 
   Session.findById(req.headers.authorization)
-    .populate({ path: "userId", model: "User" })
+    .populate({ path: "user", model: "User" })
     .then((session) => {
       if (!session) {
         return res
