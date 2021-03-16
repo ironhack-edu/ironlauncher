a388b61de75b0ec05fd647757758c4f25f03bc86
Filipe <filipefreire@protonmail.com>
2021-03-16 20:42:39 +0000

    -Update package name & html titles in the templates

--- template/views/base/app.js
+++ template/views/base/app.js
@@ -1,34 +1,34 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require('hbs');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require("./config")(app);
+require('./config')(app);
 
 // default value for title local
-const projectName = "{{name}}";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = '{{name}}';
+const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)}- Generated with IronGenerator`;
+app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require("./routes/index");
-app.use("/", index);
+const index = require('./routes/index');
+app.use('/', index);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
