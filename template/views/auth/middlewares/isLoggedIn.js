dac690a7a449a1c228857115ca10d2fb44e76943
André de Albuquerque <itstheandre@gmail.com>
2021-04-06 16:05:01 +0200

    fix: wrong middleware implementation in views/auth

--- template/views/auth/middlewares/isLoggedIn.js
+++ template/views/auth/middlewares/isLoggedIn.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
   // checks if the user is logged in when trying to access a specific page
   if (!req.session.user) {
-    return res.redirect('/login');
+    return res.redirect("/auth/login");
   }
   req.user = req.session.user;
   next();
