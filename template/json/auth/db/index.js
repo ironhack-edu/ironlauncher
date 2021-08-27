fc21c507763722616544f1f0bd366dbb013eec1a
André de Albuquerque <itstheandre@gmail.com>
2021-08-27 08:22:53 +0200

    fix Mongoose error caused by no longer expecting the flags

--- template/json/auth/db/index.js
+++ template/json/auth/db/index.js
@@ -8,12 +8,7 @@ const mongoose = require("mongoose");
 const MONGO_URI = require("../utils/const");
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
+  .connect(MONGO_URI)
   .then((x) => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
