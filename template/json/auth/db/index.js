f0797fd4318e41318d5ebbc57e0f1ffac36b1c32
André de Albuquerque <itstheandre@gmail.com>
2021-11-20 13:34:48 +0100

    fix: bad paths in json/auth part 2 - revenge of the plural strikes again

--- template/json/auth/db/index.js
+++ template/json/auth/db/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = require("../utils/const");
+const MONGO_URI = require("../utils/consts");
 
 mongoose
   .connect(MONGO_URI)
