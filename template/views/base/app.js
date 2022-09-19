141debf148d0c0f07263fa1974c35cc897b11d0d
ross-u <cirkovic.uros.srb@gmail.com>
2022-09-19 19:16:15 +0200

    fix: views/base - update dotenv import and config syntax

--- template/views/base/app.js
+++ template/views/base/app.js
@@ -1,6 +1,6 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require("dotenv").config();
 
 // ℹ️ Connects to the database
 require("./db");
