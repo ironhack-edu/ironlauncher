b24ed7c60e50e28fea6b05813046dc15e1b6f494
André de Albuquerque <itstheandre@gmail.com>
2022-09-20 20:35:41 +0200

    Before trip

--- source/lib/fs/isDirectory/is-directory.test.ts
+++ source/lib/fs/isDirectory/is-directory.test.ts
@@ -1,5 +1,44 @@
-import { describe } from "vitest";
+import { afterEach, assert, describe, expect, it } from "vitest";
+import mockFS from "mock-fs";
+import { join } from "path";
+import { makeDirectoryExists } from "./isDirectory";
 
-describe('isDirectory', () => {
-	
-})
+describe("isDirectory", () => {
+  describe("with mocked filesystem", () => {
+    afterEach(() => {
+      mockFS.restore();
+    });
+
+    it("should work if target address is directory", () => {
+      mockFS({ here: {} });
+
+      const func = makeDirectoryExists();
+
+      const result = func("here");
+
+      assert(result.isOk());
+    });
+
+    it("should return false if target address is not a directory", () => {
+      mockFS({ here: "" });
+
+      const func = makeDirectoryExists();
+
+      const result = func("here");
+
+      assert(result.isOk());
+
+      expect(result.get()).toBe(false);
+    });
+
+    it("should return error if target dir does not exist", () => {
+      mockFS({});
+
+      const func = makeDirectoryExists();
+
+      const result = func("here");
+
+      assert(result.isError());
+    });
+  });
+});
--- source/lib/fs/isDirectory/isDirectory.ts
+++ source/lib/fs/isDirectory/isDirectory.ts
@@ -1,35 +1,26 @@
 import { Result } from "@swan-io/boxed";
 import { lstatSync, Stats } from "node:fs";
+import { fromTruthy } from "../../option-wrapper";
 
 type IsDirFunc = (target: string) => Stats;
 
-type ISDirDeps = (
-  isDir?: IsDirFunc
-) => (target: string) => Result<boolean, unknown>;
-
-const makeIsDirectory: ISDirDeps =
-  (isDir = lstatSync) =>
-  (target) =>
-    Result.fromExecution(() => isDir(target)).map((val) => val.isDirectory());
-
-export function isDirectoy(
-  target: string
-): Result<boolean, NodeJS.ErrnoException> {
-  const lsStatResult = Result.fromExecution<Stats, NodeJS.ErrnoException>(() =>
-    lstatSync(target)
-  ).map((val) => val.isDirectory());
-
-  return lsStatResult;
-}
-
-type IDirectoryExists = (
+export type IDirectoryExists = (
   targetDir?: IsDirFunc
-) => (target: string) => Result<boolean, NodeJS.ErrnoException>;
+) => (
+  target: string
+) => Result<boolean, NodeJS.ErrnoException | NoTargetProvided>;
 
-const makeDirectoryExists: IDirectoryExists =
+class NoTargetProvided extends Error {}
+
+export const makeDirectoryExists: IDirectoryExists =
   (isDirFunc = lstatSync) =>
   (target) => {
-    return Result.fromExecution<Stats, NodeJS.ErrnoException>(() =>
-      isDirFunc(target)
-    ).map((val) => val.isDirectory());
+    return fromTruthy(target)
+      .toResult(new NoTargetProvided())
+      .flatMap((target) =>
+        Result.fromExecution<Stats, NodeJS.ErrnoException>(() =>
+          isDirFunc(target)
+        )
+      )
+      .map((val) => val.isDirectory());
   };
