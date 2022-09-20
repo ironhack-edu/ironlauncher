c60e75d8ec33f0247e01ee2e600bacf834756114
ross-u <cirkovic.uros.srb@gmail.com>
2022-09-20 12:48:21 +0200

    fix: views/auth - add mdn documentation reference

--- template/views/authentication/public/js/script.js
+++ template/views/authentication/public/js/script.js
@@ -1,7 +1,4 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("{{name}} JS imported successfully!");
-  },
-  false
-);
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("{{name}} JS imported successfully!");
+});
