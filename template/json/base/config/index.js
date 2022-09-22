33f11a12430382bac8635960abfae139d42f5a43
ross-u <cirkovic.uros.srb@gmail.com>
2022-09-22 17:02:04 +0200

    fix: json/base - update cors options

--- template/json/base/config/index.js
+++ template/json/base/config/index.js
@@ -13,6 +13,8 @@ const cookieParser = require("cookie-parser");
 // unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
+const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
+
 // Middleware configuration
 module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
@@ -23,7 +25,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
+      origin: [FRONTEND_URL]
     })
   );
 
