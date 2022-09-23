ea202fc37e103d7ee72bf7e97b3790941bc87066
ross-u <cirkovic.uros.srb@gmail.com>
2022-09-23 11:40:32 +0200

    fix: json/auth - move `auth.routes.js` import to `app.js`

--- template/json/authentication/app.js
+++ template/json/authentication/app.js
@@ -15,9 +15,11 @@ const app = express();
 require("./config")(app);
 
 // 👇 Start handling routes here
-// Contrary to the views version, all routes are controlled from the routes/index.js
-const allRoutes = require("./routes/index.routes");
-app.use("/api", allRoutes);
+const indexRoutes = require("./routes/index.routes");
+app.use("/api", indexRoutes);
+
+const authRoutes = require("./routes/auth.routes");
+router.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- template/json/authentication/routes/index.routes.js
+++ template/json/authentication/routes/index.routes.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
-const authRoutes = require("./auth.routes");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
-
-router.use("/auth", authRoutes);
 
 module.exports = router;
