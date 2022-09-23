87289bdd6bf2ad80598c73a7bcbe3988ed9afc73
ross-u <cirkovic.uros.srb@gmail.com>
2022-09-23 13:01:37 +0200

    fix: update the error handling middleware in all json templates

--- template/json/authentication-sessions/error-handling/index.js
+++ template/json/authentication-sessions/error-handling/index.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
     // this middleware runs whenever requested page is not available
-    res.status(404).json({ errorMessage: "This route does not exist" });
+    res.status(404).json({ message: "This route does not exist" });
   });
 
   app.use((err, req, res, next) => {
@@ -12,7 +12,7 @@ module.exports = (app) => {
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
       res.status(500).json({
-        errorMessage: "Internal server error. Check the server console",
+        message: "Internal server error. Check the server console",
       });
     }
   });
