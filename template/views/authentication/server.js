56d4c150cbbeebbb7c680c6ea17e1fc31720e8a8
ross-u <cirkovic.uros.srb@gmail.com>
2022-09-29 14:12:47 +0200

    fix: all templates - update the server start message

--- template/views/authentication/server.js
+++ template/views/authentication/server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
