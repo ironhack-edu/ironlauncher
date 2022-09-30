1fae7621d9d0afa7f90acfec7cf558fb9e6eaa67
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2022-09-30 14:23:23 +0200

    Update M2 templates (#45)
    * fix: views/base - update mongo connection string
    
    * fix: views/base - add email field and validators to the  user model
    
    * fix: views/base - add mdn documentation reference
    
    * fix: views/base - update express router import in index.routes.js
    - prefer descriptive and legible over brief and illegible
    
    * fix: views/base - update function name
    update the existing function name from ~~capitalized~~ -> capitalize
    
    * fix: views/base - update not found error message
    
    * fix: views/base - update dotenv import and config syntax
    
    * fix: views/base - change variable name
    - prefer descriptive and legible over brief and illegible
    
    * fix: views/base - update server start message
    
    * fix: views/auth - update mongo connection string
    - update mongo connection string in 'db/index.js'
    - remove the file 'utils/consts.js' for consistency with views/base
    
    * fix: views/auth - rename the user object stored in req.session
    - rename `req.session.user` to `req.session.currentUser` to align with the lesson
    - remove the `req.user` property to align with the lesson
    
    * fix: views/auth - add email field and validators to the user model
    
    * fix: views/auth - add mdn documentation reference
    
    * fix: views/auth - update express router import in `index.routes.js` and `auth.routes.js`
        - prefer descriptive and legible over brief and illegible
    
    * fix: views/auth - update utility function name
    - update the existing function name from ~~capitalized~~ -> capitalize
    
    * fix: views/auth - change variable name
    - prefer descriptive and legible over brief and illegible
    
    * fix: views/auth - update server start message
    
    * fix: views/auth - add email field to sign-up and log-in forms
    
    * fix: views/auth - update comments in auth.routes.js
    
    * fix: views/auth - update dotenv import and config syntax
    
    * fix: views/auth - update the route `POST` `/auth/signup`
    
    * fix: views/auth - update the route `POST` `/auth/login`
    
    * fix: views/auth - update the route `GET` `/auth/logout`
    
    * fix: views/auth - update mongo connection string
    
    - Remove the remaining import from `config/index.js`

--- template/views/authentication/routes/auth.routes.js
+++ template/views/authentication/routes/auth.routes.js
@@ -1,10 +1,11 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
 
 // ℹ️ Handles password encryption
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
-// How many rounds should bcrypt run the salt (default [10 - 12 rounds])
+// How many rounds should bcrypt run the salt (default - 10 rounds)
 const saltRounds = 10;
 
 // Require the User model in order to interact with the database
@@ -14,140 +15,142 @@ const User = require("../models/User.model");
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
+
+// GET /auth/signup
 router.get("/signup", isLoggedOut, (req, res) => {
   res.render("auth/signup");
 });
 
+
+// POST /auth/signup
 router.post("/signup", isLoggedOut, (req, res) => {
-  const { username, password } = req.body;
+  const { username, email, password } = req.body;
 
-  if (!username) {
-    return res.status(400).render("auth/signup", {
-      errorMessage: "Please provide your username.",
-    });
+  // Check that username, email, and password are provided
+  if (username === "" || email === "" || password === "") {
+    res
+      .status(400)
+      .render("auth/signup", {
+        errorMessage: "All fields are mandatory. Please provide your username, email and password."
+      });
+
+    return;
   }
 
-  if (password.length < 8) {
-    return res.status(400).render("auth/signup", {
-      errorMessage: "Your password needs to be at least 8 characters long.",
-    });
+  if (password.length < 6) {
+    res
+      .status(400)
+      .render("auth/signup", {
+        errorMessage: "Your password needs to be at least 6 characters long.",
+      });
+
+    return;
   }
 
-  //   ! This use case is using a regular expression to control for special characters and min length
+  //   ! This regular expression checks password for special characters and minimum length
   /*
-  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!regex.test(password)) {
-    return res.status(400).render("signup", {
-      errorMessage:
-        "Password needs to have at least 8 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    res
+      .status(400)
+      .render("auth/signup", {
+        errorMessage: "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter."
     });
+    return;
   }
   */
 
-  // Search the database for a user with the username submitted in the form
-  User.findOne({ username }).then((found) => {
-    // If the user is found, send the message username is taken
-    if (found) {
-      return res
-        .status(400)
-        .render("auth.signup", { errorMessage: "Username already taken." });
-    }
-
-    // if user is not found, create a new user - start with hashing the password
-    return bcrypt
-      .genSalt(saltRounds)
-      .then((salt) => bcrypt.hash(password, salt))
-      .then((hashedPassword) => {
-        // Create a user and save it in the database
-        return User.create({
-          username,
-          password: hashedPassword,
+  // Create a new user - start by hashing the password
+  bcrypt.genSalt(saltRounds)
+    .then((salt) => bcrypt.hash(password, salt))
+    .then((hashedPassword) => {
+      // Create a user and save it in the database
+      return User.create({ username, email, password: hashedPassword });
+    })
+    .then((user) => {
+      res.redirect("/auth/login");
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: error.message });
+      }
+      else if (error.code === 11000) {
+        res.status(500).render('auth/signup', {
+            errorMessage: 'Username and email need to be unique. Provide a valid username or email.'
         });
-      })
-      .then((user) => {
-        // Bind the user to the session object
-        req.session.user = user;
-        res.redirect("/");
-      })
-      .catch((error) => {
-        if (error instanceof mongoose.Error.ValidationError) {
-          return res
-            .status(400)
-            .render("auth/signup", { errorMessage: error.message });
-        }
-        if (error.code === 11000) {
-          return res.status(400).render("auth/signup", {
-            errorMessage:
-              "Username need to be unique. The username you chose is already in use.",
-          });
-        }
-        return res
-          .status(500)
-          .render("auth/signup", { errorMessage: error.message });
-      });
-  });
+      } else {
+        next(error);
+      }
+    });
 });
 
+
+// GET /auth/login
 router.get("/login", isLoggedOut, (req, res) => {
   res.render("auth/login");
 });
 
-router.post("/login", isLoggedOut, (req, res, next) => {
-  const { username, password } = req.body;
 
-  if (!username) {
-    return res.status(400).render("auth/login", {
-      errorMessage: "Please provide your username.",
+// POST /auth/login
+router.post("/login", isLoggedOut, (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  // Check that username, email, and password are provided
+  if (username === "" || email === "" || password === "") {
+    res.status(400).render("auth/login", {
+      errorMessage: "All fields are mandatory. Please provide username, email and password."
     });
+
+    return;
   }
 
   // Here we use the same logic as above
   // - either length based parameters or we check the strength of a password
-  if (password.length < 8) {
+  if (password.length < 6) {
     return res.status(400).render("auth/login", {
-      errorMessage: "Your password needs to be at least 8 characters long.",
+      errorMessage: "Your password needs to be at least 6 characters long.",
     });
   }
 
-  // Search the database for a user with the username submitted in the form
-  User.findOne({ username })
+  // Search the database for a user with the email submitted in the form
+  User.findOne({ email })
     .then((user) => {
-      // If the user isn't found, send the message that user provided wrong credentials
+      // If the user isn't found, send an error message that user provided wrong credentials
       if (!user) {
-        return res.status(400).render("auth/login", {
-          errorMessage: "Wrong credentials.",
-        });
+        res.status(400).render("auth/login", { errorMessage: "Wrong credentials." });
+        return;
       }
 
       // If user is found based on the username, check if the in putted password matches the one saved in the database
-      bcrypt.compare(password, user.password).then((isSamePassword) => {
-        if (!isSamePassword) {
-          return res.status(400).render("auth/login", {
-            errorMessage: "Wrong credentials.",
-          });
-        }
-        req.session.user = user;
-        // req.session.user = user._id; // ! better and safer but in this case we saving the entire user object
-        return res.redirect("/");
-      });
-    })
+      bcrypt.compare(password, user.password)
+        .then((isSamePassword) => {
+          if (!isSamePassword) {
+            res.status(400).render("auth/login", { errorMessage: "Wrong credentials." });
+            return;
+          }
 
-    .catch((err) => {
-      // in this case we are sending the error handling to the error handling middleware that is defined in the error handling file
-      // you can just as easily run the res.status that is commented out below
-      next(err);
-      // return res.status(500).render("login", { errorMessage: err.message });
-    });
+          // Add the user object to the session object
+          req.session.currentUser = user.toObject();
+          // Remove the password field
+          delete req.session.currentUser.password;
+
+          res.redirect("/");
+        })
+        .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
+
+    })
+    .catch((err) => next(err));
 });
 
+
+// GET /auth/logout
 router.get("/logout", isLoggedIn, (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      return res
-        .status(500)
-        .render("auth/logout", { errorMessage: err.message });
+      res.status(500).render("auth/logout", { errorMessage: err.message });
+      return;
     }
+    
     res.redirect("/");
   });
 });
--- template/views/authentication/utils/consts.js
+++
@@ -1,3 +0,0 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/{{name}}";
-
-module.exports = MONGO_URI;
