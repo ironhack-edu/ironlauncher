c2e6b69e73a4f262b08628792b7f4d17d5f7c971
André de Albuquerque <itstheandre@gmail.com>
2022-10-15 14:13:34 +0200

    fix: unnecessary imports

--- template/fullstack/authentication/client/src/context/auth.context.jsx
+++ template/fullstack/authentication/client/src/context/auth.context.jsx
@@ -1,28 +1,25 @@
 import React, { useState, useEffect } from "react";
 import authService from "../services/auth.service";
-import axios from "axios";
-
 
 const AuthContext = React.createContext();
-
 
 function AuthProviderWrapper(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
-  
+
   const storeToken = (token) => {
     localStorage.setItem("authToken", token);
-  }  
-    
-  const authenticateUser = () => { 
+  };
+
+  const authenticateUser = () => {
     // Get the stored token from the localStorage
     const storedToken = localStorage.getItem("authToken");
-    
+
     // If the token exists in the localStorage
     if (storedToken) {
-        // Send a request to the server using axios
-        /* 
+      // Send a request to the server using axios
+      /* 
         axios.get(
           `${process.env.REACT_APP_SERVER_URL}/auth/verify`,
           { headers: { Authorization: `Bearer ${storedToken}` } }
@@ -30,42 +27,41 @@ function AuthProviderWrapper(props) {
         .then((response) => {})
         */
 
-        // Or using a service
-      authService.verify()
+      // Or using a service
+      authService
+        .verify()
         .then((response) => {
           // If the server verifies that JWT token is valid  ✅
           const user = response.data;
-        // Update state variables        
+          // Update state variables
           setIsLoggedIn(true);
           setIsLoading(false);
           setUser(user);
         })
         .catch((error) => {
           // If the server sends an error response (invalid token) ❌
-          // Update state variables        
+          // Update state variables
           setIsLoggedIn(false);
           setIsLoading(false);
           setUser(null);
         });
-
     } else {
       // If the token is not available
       setIsLoggedIn(false);
       setIsLoading(false);
       setUser(null);
     }
-  }
+  };
 
   const removeToken = () => {
     localStorage.removeItem("authToken");
-  }    
-  
+  };
+
   const logOutUser = () => {
-    // Upon logout, remove the token from the localStorage    
+    // Upon logout, remove the token from the localStorage
     removeToken();
     authenticateUser();
-  }    
-
+  };
 
   useEffect(() => {
     // Run this code once the AuthProviderWrapper component in the App loads for the first time.
@@ -75,11 +71,18 @@ function AuthProviderWrapper(props) {
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn, isLoading, user, storeToken, authenticateUser, logOutUser }}
+      value={{
+        isLoggedIn,
+        isLoading,
+        user,
+        storeToken,
+        authenticateUser,
+        logOutUser,
+      }}
     >
       {props.children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 export { AuthProviderWrapper, AuthContext };
