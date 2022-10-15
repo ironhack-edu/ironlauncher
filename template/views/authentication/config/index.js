dca1d9202429568cf121c7cc996b4b522b10dfc9
André de Albuquerque <itstheandre@gmail.com>
2022-10-15 14:18:57 +0200

    fix: localhost to allow for cross platform ironlaunching

--- template/views/authentication/config/index.js
+++ template/views/authentication/config/index.js
@@ -27,7 +27,7 @@ const MongoStore = require("connect-mongo");
 
 // Connects the mongo uri to maintain the same naming structure
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/{{name}}";
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/{{name}}";
 
 // Middleware configuration
 module.exports = (app) => {
